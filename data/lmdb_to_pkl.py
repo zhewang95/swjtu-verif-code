d790c149822466f02c2452086ff3b4ffe72f5dad
wangzhe <wangzhe140@163.com>
2017-02-02 18:09:29 +0800

    test

--- data/lmdb_to_pkl.py
+++ data/lmdb_to_pkl.py
@@ -4,15 +4,15 @@ import lmdb,cPickle,caffe
 import numpy as np
 
 def main():
-    #env=lmdb.open('/home/wz/project/caffe/examples/mnist/mnist_train_lmdb',readonly=True)
-    env=lmdb.open('swjtu_verif_test_lmdb',readonly=True)
+    env=lmdb.open('/home/wz/project/caffe/examples/mnist/mnist_train_lmdb',readonly=True)
+    #env=lmdb.open('swjtu_verif_test_lmdb',readonly=True)
     with env.begin() as txn:
         raw_datum=txn.get(b'00000000')
 
     datum=caffe.proto.caffe_pb2.Datum()
     datum.ParseFromString(raw_datum)
 
-    flat_x=np.fromstring(datum.data,dtype=np.float64)
+    flat_x=np.fromstring(datum.data,dtype=np.uint8)
     x=flat_x.reshape(datum.channels,datum.height,datum.width)
     y=datum.label
     print x,y
